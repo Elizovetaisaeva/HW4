901a2c82c769b49cdbc3d9c726742d7470952f36
Лаисат <lz_256@mail.ru>
2023-10-27 00:28:37 +0300

    HW4 comment

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-
+        task2();
+        task3();
+        task4();
+        task5();
     }
 
 
@@ -20,5 +23,48 @@ public class Main {
         System.out.println("double d = "+"10.896F");
 
 
+    }
+    public static void task2() {
+        byte a = (byte)-159;
+        System.out.println("byte a = "+"-159");
+        short b = 569;
+        System.out.println("short b = "+"569");
+        int c = 27897;
+        System.out.println("int c = "+ "27897");
+        long d = 987-678-965-549;
+        System.out.println("long d = "+"987678965549");
+        float e = 2.786f;
+        System.out.println("float e = "+"2.786f");
+        double g = 27.12F;
+        System.out.println("double g = "+"27.12F");
+
+    }
+
+
+    public static void task3() {
+        byte students = (27+23)+30;
+        System.out.println("Количество учеников  = " + students);
+        byte paper = 480/80;
+        System.out.println("На каждого ученика рассчитано  " + paper + " листов бумаги");
+
+
+    }
+
+    public static void task4() {
+
+
+    }
+
+    public static void task5() {
+        byte cansPaint = 120;
+        System.out.println("cansPaint  = "+"120");
+        short whiteBrown = 2+4;
+        System.out.println("На один класс уходит = " + whiteBrown);
+        byte cans = 120/6;
+        System.out.println("количество = " + cans);
+        byte white = 20*2;
+        System.out.println("банки белой краски = " + white);
+        byte Brown = 20*4;
+        System.out.println("банки коричневой краски = " + Brown);
     }
 }
