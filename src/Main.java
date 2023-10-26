4e2793f09747911bab3b3e91f44b7f8d45b00070
Лаисат <lz_256@mail.ru>
2023-10-26 22:10:55 +0300

    HW4 comment

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,18 @@ public class Main {
 
     public static void task1() {
         byte grape = 21;
-        System.out.println("виноград" +grape+ "кг");
-
+        System.out.println("byte grape=" + "21");
         short apples = 231;
-        System.out.println("яблоки" +apples+ "кг");
+        System.out.println("short apples=" + "231");
+        int a = 30000;
+        System.out.println("int a =" + "30000");
+        long b = 976789654;
+        System.out.println("long b="+"976789654");
+        float c = 4.55f;
+        System.out.println("float c ="+"4.55f");
+        double d = 10.896F;
+        System.out.println("double d ="+"10.896F");
+
 
     }
 }
