1ea254ea16023d03d20562fb5cf2f5858cfc4f78
Лаисат <lz_256@mail.ru>
2023-10-26 21:46:37 +0300

    HW4 comment

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,11 @@ public class Main {
 
 
     public static void task1() {
+        byte grape = 21;
+        System.out.println("виноград" +grape+ "кг");
 
+        short apples = 231;
+        System.out.println("яблоки" +apples+ "кг");
 
     }
 }
